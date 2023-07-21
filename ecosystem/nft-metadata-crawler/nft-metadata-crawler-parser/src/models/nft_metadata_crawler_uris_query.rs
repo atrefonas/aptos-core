e685d29436580027acdd84771c753162f217e88a
Justin Chang <justin@aptoslabs.com>
2023-07-21 12:18:17 -0700

    backoff crate

--- ecosystem/nft-metadata-crawler/nft-metadata-crawler-parser/src/models/nft_metadata_crawler_uris_query.rs
+++ ecosystem/nft-metadata-crawler/nft-metadata-crawler-parser/src/models/nft_metadata_crawler_uris_query.rs
@@ -1,12 +1,16 @@
 // Copyright © Aptos Foundation
 
+use std::time::Duration;
+
 use crate::schema::nft_metadata_crawler_uris;
+use backoff::{retry, ExponentialBackoff};
 use diesel::{
     prelude::*,
     r2d2::{ConnectionManager, PooledConnection},
 };
 use serde::{Deserialize, Serialize};
-use std::{thread, time::Duration};
+
+const MAX_RETRY_TIME_SECONDS: u64 = 15;
 
 #[derive(Debug, Deserialize, Identifiable, Queryable, Serialize)]
 #[diesel(primary_key(token_uri))]
@@ -28,74 +32,77 @@ impl NFTMetadataCrawlerURIsQuery {
     pub fn get_by_token_uri(
         token_uri: String,
         conn: &mut PooledConnection<ConnectionManager<PgConnection>>,
-    ) -> diesel::QueryResult<Option<Self>> {
-        for _ in 0..3 {
-            match nft_metadata_crawler_uris::table
+    ) -> anyhow::Result<Option<Self>> {
+        let mut op = || {
+            nft_metadata_crawler_uris::table
                 .find(token_uri.clone())
                 .first::<NFTMetadataCrawlerURIsQuery>(conn)
                 .optional()
-            {
-                Ok(result) => return Ok(result),
-                Err(_) => thread::sleep(Duration::from_secs(5)),
-            }
-        }
+                .map_err(Into::into)
+        };
 
-        // Retry one last time but now propagate the error if it fails
-        nft_metadata_crawler_uris::table
-            .find(token_uri)
-            .first::<NFTMetadataCrawlerURIsQuery>(conn)
-            .optional()
+        let backoff = ExponentialBackoff {
+            max_elapsed_time: Some(Duration::from_secs(MAX_RETRY_TIME_SECONDS)),
+            ..Default::default()
+        };
+
+        match retry(backoff, &mut op) {
+            Ok(result) => Ok(result),
+            Err(_) => Ok(op()?),
+        }
     }
 
     pub fn get_by_raw_image_uri(
         raw_image_uri: Option<String>,
         conn: &mut PooledConnection<ConnectionManager<PgConnection>>,
-    ) -> diesel::QueryResult<Option<Self>> {
+    ) -> anyhow::Result<Option<Self>> {
         if raw_image_uri.is_none() {
             return Ok(None);
         }
 
-        for _ in 0..3 {
-            match nft_metadata_crawler_uris::table
+        let mut op = || {
+            nft_metadata_crawler_uris::table
                 .filter(nft_metadata_crawler_uris::raw_image_uri.eq(raw_image_uri.clone()))
                 .first::<NFTMetadataCrawlerURIsQuery>(conn)
                 .optional()
-            {
-                Ok(result) => return Ok(result),
-                Err(_) => thread::sleep(Duration::from_secs(5)),
-            }
-        }
+                .map_err(Into::into)
+        };
 
-        // Retry one last time but now propagate the error if it fails
-        nft_metadata_crawler_uris::table
-            .filter(nft_metadata_crawler_uris::raw_image_uri.eq(raw_image_uri))
-            .first::<NFTMetadataCrawlerURIsQuery>(conn)
-            .optional()
+        let backoff = ExponentialBackoff {
+            max_elapsed_time: Some(Duration::from_secs(MAX_RETRY_TIME_SECONDS)),
+            ..Default::default()
+        };
+
+        match retry(backoff, &mut op) {
+            Ok(result) => Ok(result),
+            Err(_) => Ok(op()?),
+        }
     }
 
     pub fn get_by_raw_animation_uri(
         raw_animation_uri: Option<String>,
         conn: &mut PooledConnection<ConnectionManager<PgConnection>>,
-    ) -> diesel::QueryResult<Option<Self>> {
+    ) -> anyhow::Result<Option<Self>> {
         if raw_animation_uri.is_none() {
             return Ok(None);
         }
 
-        for _ in 0..3 {
-            match nft_metadata_crawler_uris::table
+        let mut op = || {
+            nft_metadata_crawler_uris::table
                 .filter(nft_metadata_crawler_uris::raw_animation_uri.eq(raw_animation_uri.clone()))
                 .first::<NFTMetadataCrawlerURIsQuery>(conn)
                 .optional()
-            {
-                Ok(result) => return Ok(result),
-                Err(_) => thread::sleep(Duration::from_secs(5)),
-            }
-        }
+                .map_err(Into::into)
+        };
 
-        // Retry one last time but now propagate the error if it fails
-        nft_metadata_crawler_uris::table
-            .filter(nft_metadata_crawler_uris::raw_animation_uri.eq(raw_animation_uri))
-            .first::<NFTMetadataCrawlerURIsQuery>(conn)
-            .optional()
+        let backoff = ExponentialBackoff {
+            max_elapsed_time: Some(Duration::from_secs(MAX_RETRY_TIME_SECONDS)),
+            ..Default::default()
+        };
+
+        match retry(backoff, &mut op) {
+            Ok(result) => Ok(result),
+            Err(_) => Ok(op()?),
+        }
     }
 }
